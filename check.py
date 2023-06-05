9703799e61d22af57d0714ae44ab3007d8486264
Quyyyyy <82216233+Quyyyyy@users.noreply.github.com>
2023-06-05 22:09:44 +0700

    my first commit

--- check.py
+++ check.py
@@ -37,3 +37,42 @@ spectral_centroids = librosa.feature.spectral_centroid(y=audio, sr=sr)
 
 # Print the resulting spectral centroids
 print("Spectral Centroids:", spectral_centroids)
+
+
+
+
+
+# import numpy as np
+# import scipy.io.wavfile as wav
+
+# def calculate_low_energy(signal, window_size, threshold):
+#     energy = np.sum(signal ** 2)  # Tính năng lượng tổng của toàn bộ tín hiệu
+#     low_energy_frames = []
+
+#     num_frames = len(signal) // window_size  # Số lượng khung hình trong tín hiệu
+
+#     for i in range(num_frames):
+#         start = i * window_size
+#         end = start + window_size
+#         frame = signal[start:end]
+#         frame_energy = np.sum(frame ** 2)  # Tính năng lượng của từng khung hình
+
+#         if frame_energy / energy < threshold:
+#             low_energy_frames.append(frame)
+
+#     return low_energy_frames
+
+# # Đọc tệp âm thanh
+# sample_rate, signal = wav.read("audio_file.wav")
+
+# # Chuyển đổi tín hiệu thành dạng số thực và chuẩn hóa
+# signal = signal.astype(np.float32) / 32767.0
+
+# # Cấu hình các thông số
+# window_size = 1024  # Kích thước của mỗi khung hình
+# threshold = 0.1  # Ngưỡng năng lượng thấp
+
+# # Tính toán low energy frames
+# low_energy_frames = calculate_low_energy(signal, window_size, threshold)
+
+# print("Low Energy Frames:", low_energy_frames)
